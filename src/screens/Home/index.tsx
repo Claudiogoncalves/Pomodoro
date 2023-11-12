07def65bd9c8c64b525abb40ea8531bc15f35b1e
Claudio Roberto Prerira Gonçalves <claudiogonclavesrp@gmail.com>
2023-11-12 17:43:37 +0000

    Merge master

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,46 @@
+import { Play } from "phosphor-react";
+import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput 
+            id="task" 
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value=""></option>
+          </datalist>
+
+          <label htmlFor="">durante</label>
+          <MinutesAmountInput 
+            type="number" 
+            id="minutesAmount" 
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountdownButton disabled={false} type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
